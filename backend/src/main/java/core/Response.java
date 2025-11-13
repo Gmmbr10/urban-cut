d6b89135a16ec660bbce925fe6838c060d4410b5
giovanne <giovannemmelo@gmail.com>
2025-11-13 20:02:12 -0300

    feat: criando um padrão para as respostas dos services

---
+++ backend/src/main/java/core/Response.java
@@ -0,0 +1,60 @@
+package core;
+
+public class Response<T> {
+    private Integer statusCode = null;
+    private String mensagem = "";
+    private T dado = null;
+
+    public Response(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Response(int statusCode, String mensagem) {
+        this.statusCode = statusCode;
+        this.mensagem = mensagem;
+    }
+
+    public Response(int statusCode, T dado) {
+        this.statusCode = statusCode;
+        this.dado = dado;
+    }
+
+    public Response(int statusCode, String mensagem, T dado) {
+        this.statusCode = statusCode;
+        this.mensagem = mensagem;
+        this.dado = dado;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "statusCode=" + statusCode +
+                ", mensagem='" + mensagem + '\'' +
+                ", dado=" + dado +
+                '}';
+    }
+}
