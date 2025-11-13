463b4bfca252db0ff934f5565277dac732f6dbaa
giovanne <giovannemmelo@gmail.com>
2025-11-13 20:00:49 -0300

    feat: criando os models (Barbeiro, Cliente)

---
+++ backend/src/main/java/models/Barbeiro.java
@@ -0,0 +1,87 @@
+package models;
+
+import core.Model;
+
+public class Barbeiro extends Model {
+    private Integer idBarbeiro;
+    private String nome;
+    private String email;
+    private String senha;
+    private Integer idBarbearia;
+
+    private Barbeiro(Integer idBarbeiro, String nome, String email, String senha, Integer idBarbearia) {
+        this.idBarbeiro = idBarbeiro;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.idBarbearia = idBarbearia;
+    }
+
+    public static class BarbeiroBuilder {
+        private Integer idBarbeiro = null;
+        private String nome = null;
+        private String email = null;
+        private String senha = null;
+        private Integer idBarbearia = null;
+
+        public BarbeiroBuilder idBarbeiro(int id) {
+            this.idBarbeiro = id;
+            return this;
+        }
+
+        public BarbeiroBuilder nome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public BarbeiroBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public BarbeiroBuilder senha(String senha) {
+            this.senha = senha;
+            return this;
+        }
+
+        public BarbeiroBuilder idBarbearia(int id) {
+            this.idBarbearia = id;
+            return this;
+        }
+
+        public Barbeiro build() {
+            return new Barbeiro(idBarbeiro, nome, email, senha, idBarbearia);
+        }
+    }
+
+    public Integer getIdBarbeiro() {
+        return idBarbeiro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public Integer getIdBarbearia() {
+        return idBarbearia;
+    }
+
+    @Override
+    public String toString() {
+        return "Barbeiro{" +
+                "idBarbeiro=" + idBarbeiro +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", idBarbearia=" + idBarbearia +
+                '}';
+    }
+}
