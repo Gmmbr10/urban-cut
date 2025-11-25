f68f4f3465c1810ad5e86240076b2b2917a2f40d
giovanne <giovannemmelo@gmail.com>
2025-11-25 17:47:45 -0300

    feat: criando o método que verifica se está dentro do horário da barbearia

--- backend/src/main/java/com/urbancut/services/AtendimentoService.java
+++ backend/src/main/java/com/urbancut/services/AtendimentoService.java
@@ -3,15 +3,21 @@ package com.urbancut.services;
 import com.urbancut.core.Response;
 import com.urbancut.core.Service;
 import com.urbancut.models.Atendimento;
+import com.urbancut.models.Barbearia;
 import com.urbancut.models.Barbeiro;
+import com.urbancut.models.DiaFuncionamento;
 import com.urbancut.repositories.AtendimentoRepository;
+import com.urbancut.repositories.BarbeariaRepository;
 import com.urbancut.repositories.BarbeiroRepository;
+import com.urbancut.repositories.DiaFuncionamentoRepository;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.sql.SQLException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 public class AtendimentoService extends Service<AtendimentoRepository> {
     public AtendimentoService() {
@@ -90,5 +96,64 @@ public class AtendimentoService extends Service<AtendimentoRepository> {
         boolean isDentroDoHorarioBloqueado = barbeiro.getHorarioBloqueado().getInicio().isBefore(dataHora.toLocalTime()) && barbeiro.getHorarioBloqueado().getFim().isAfter(dataHora.toLocalTime());
 
         return isDentroDoHorarioBloqueado;
+    }
+
+    private boolean isDentroDoHorarioDaBarbearia(LocalDateTime dataHora, int idBarbearia) throws SQLException {
+        Barbearia barbearia = new BarbeariaRepository().searchById(idBarbearia);
+
+        if (barbearia == null) return false;
+
+        List<DiaFuncionamento> diasFuncionamento = new DiaFuncionamentoRepository().searchByBarbearia(barbearia.getIdBarbearia());
+
+        DayOfWeek dia = dataHora.toLocalDate().getDayOfWeek();
+
+        for (DiaFuncionamento diaFuncionamento : diasFuncionamento) {
+            switch (dia) {
+                case SUNDAY:
+                    if (!diaFuncionamento.getDiaSemana().equals("Domingo")) {
+                        continue;
+                    }
+
+                    return diaFuncionamento.getHoraAbertura().minusMinutes(1).isBefore(dataHora.toLocalTime()) && diaFuncionamento.getHoraFechamento().isAfter(dataHora.toLocalTime());
+                case MONDAY:
+                    if (!diaFuncionamento.getDiaSemana().equals("Segunda")) {
+                        continue;
+                    }
+
+                    return diaFuncionamento.getHoraAbertura().minusMinutes(1).isBefore(dataHora.toLocalTime()) && diaFuncionamento.getHoraFechamento().isAfter(dataHora.toLocalTime());
+                case TUESDAY:
+                    if (!diaFuncionamento.getDiaSemana().equals("Terça")) {
+                        continue;
+                    }
+
+                    return diaFuncionamento.getHoraAbertura().minusMinutes(1).isBefore(dataHora.toLocalTime()) && diaFuncionamento.getHoraFechamento().isAfter(dataHora.toLocalTime());
+                case WEDNESDAY:
+                    if (!diaFuncionamento.getDiaSemana().equals("Quarta")) {
+                        continue;
+                    }
+
+                    return diaFuncionamento.getHoraAbertura().minusMinutes(1).isBefore(dataHora.toLocalTime()) && diaFuncionamento.getHoraFechamento().isAfter(dataHora.toLocalTime());
+                case THURSDAY:
+                    if (!diaFuncionamento.getDiaSemana().equals("Quinta")) {
+                        continue;
+                    }
+
+                    return diaFuncionamento.getHoraAbertura().minusMinutes(1).isBefore(dataHora.toLocalTime()) && diaFuncionamento.getHoraFechamento().isAfter(dataHora.toLocalTime());
+                case FRIDAY:
+                    if (!diaFuncionamento.getDiaSemana().equals("Sexta")) {
+                        continue;
+                    }
+
+                    return diaFuncionamento.getHoraAbertura().minusMinutes(1).isBefore(dataHora.toLocalTime()) && diaFuncionamento.getHoraFechamento().isAfter(dataHora.toLocalTime());
+                case SATURDAY:
+                    if (!diaFuncionamento.getDiaSemana().equals("Sábado")) {
+                        continue;
+                    }
+
+                    return diaFuncionamento.getHoraAbertura().minusMinutes(1).isBefore(dataHora.toLocalTime()) && diaFuncionamento.getHoraFechamento().isAfter(dataHora.toLocalTime());
+            }
+        }
+
+        return false;
     }
 }
